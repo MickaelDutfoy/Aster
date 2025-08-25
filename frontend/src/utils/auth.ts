3f3a264e176db778c0888c2124bec25b067a254b
MickaelDutfoy <m.dutfoy@gmail.com>
2025-08-25 13:19:01 +0200

    fix route axios

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,11 +1,12 @@
 import axios from "axios";
+import { api } from "../api";
 
 export async function checkToken(): Promise<boolean> {
   const token = localStorage.getItem("token");
   if (!token) return false;
 
   try {
-    await axios.get("http://localhost:3001/api/sessions", {
+    await axios.get(api("/api/sessions"), {
       headers: { Authorization: `Bearer ${token}` },
     });
     return true;
